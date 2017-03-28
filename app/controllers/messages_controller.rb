aab99ce81a9157f871c30a7309ad4b7a17acf17f
Hash14 <sgurumoorthy.28@gmail.com>
2017-03-28 16:05:03 +0530

    reply for msg to facebook bot22

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,6 +53,7 @@ class MessagesController < ApplicationController
       "recipient" => {"id": recipient},
       "message" => {"text": text}
     }), 'headers' => {'Content-type': 'application/json'})
+    puts r.text
   	# if r.status_code != requests.codes.ok
    #  	puts r.text
   	# end
