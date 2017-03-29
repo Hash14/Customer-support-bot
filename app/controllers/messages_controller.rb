a183b93885a883934da1a63dba133c1242eff705
Hash14 <sgurumoorthy.28@gmail.com>
2017-03-29 10:52:43 +0530

    request changes 12

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -62,14 +62,19 @@ class MessagesController < ApplicationController
     # data = URI.encode_www_form("params" => {"access_token": token},"data" => ({"recipient" => {"id": recipient}, "message" => {"text": text}}))
     # req.body = data
     # response = http.request(req)
-    recipient = recipient.to_s
-    text = text.to_s
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = JSON.dumps({
+        "recipient": {
+            "id": recipient
+        },
+        "message": {
+            "text": text
+        }
+    })
     uri = URI('https://graph.facebook.com/v2.6/me/messages?access_token=EAAUq9k26EawBAMJqvdqDuURTWC6hKgxIBbaUm5ZCZBZAq7yeJIG0ZBU59hCcSziFQBYjsprTiUJ5MaJKA5jF75ngI3ZChwhkhQv4qkZCyfPIZClsYQ0Yf11p7md02rpqoOZB3FFAiFUiEHZCZCELHTZBDeBS2oTZCke40n0KTxE1FJubyQZDZD')
-  	r = Net::HTTP.post_form(uri,
-    "data": ({
-      "recipient": {"id": recipient},
-      "message": {"text": text}
-    }), "headers": {'Content-type': 'application/json'})
+  	r = Net::HTTP.post_form(uri, headers=headers, data=data)
      puts r.inspect
   	# if r.status_code != requests.codes.ok
    #  	puts r.text
