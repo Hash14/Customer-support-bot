12ba479ca4cfa6f87eb2faf6375c6b8be3e0ed94
Hash14 <sgurumoorthy.28@gmail.com>
2017-03-29 09:53:27 +0530

    request changes 3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -64,7 +64,7 @@ class MessagesController < ApplicationController
     # response = http.request(req)
 
     uri = URI('https://graph.facebook.com/v2.6/me/messages?access_token=EAAUq9k26EawBAMJqvdqDuURTWC6hKgxIBbaUm5ZCZBZAq7yeJIG0ZBU59hCcSziFQBYjsprTiUJ5MaJKA5jF75ngI3ZChwhkhQv4qkZCyfPIZClsYQ0Yf11p7md02rpqoOZB3FFAiFUiEHZCZCELHTZBDeBS2oTZCke40n0KTxE1FJubyQZDZD')
-    data: ({recipient: {id: recipient}, message: {text: text} })
+    data = ({recipient: {id: recipient}, message: {text: text} })
     headers: {'Content-type': 'application/json'}
     params = data.to_json
     r = Net::HTTP.post_form(uri,params)
